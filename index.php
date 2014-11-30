d1e72f32c1a59386e6da35a30f090a4114027aef
Avi <Avi@admintheweb.com>
2014-11-30 13:52:38 +0200

    change view for two column

--- index.php
+++ index.php
@@ -14,41 +14,52 @@
 <body>
 	<div data-role="page" data-control-title="landPage" id="landPage"
 		class="mainPage">
-		<header data-role="header">
-			<div id="logo">
-			
-			</div>
+		<header>
+			<div id="logo"></div>
 			<div class="whiteLine"></div>
-			<h1 id="headerSlogen">להקה <span class="blueText">לחתונות שמחות</span></h1>
+			<h1 id="headerSlogen">
+				להקה <span class="blueText">לחתונות שמחות</span>
+			</h1>
 			<div class="blueLine"></div>
 		</header>
-		<section role="main" class="ui-content">
-		    <div id="slogen">קבלו 500 ₪ הנחה ודיסק במתנה</div>				
-			<iframe src="//www.youtube.com/embed/TgtX9gUzc90?rel=0"></iframe>
-			<section id="formTitle">
-				<br> מלא את הפרטים לקבלת ההטבה:
+		<section role="main" class="ui-content ui-grid-a ui-responsive">
+			<section id="right" class="ui-block-a">
+				<h2>מתחתנים?</h2>
+				<div>
+					<span id="slogen">לסוגרים אירוע דיסק במתנה</span>
+				</div>
+				<div id="customer">לרונן ולהקתו התותחים!! הייתם פצצה אתמול, תודה
+					שהפכתם את היום המאושר הזה לכ"כ כיפי ומקפיץ! שמחים שבחרנו אתכם
+					וכמובן נמליץ לכולם! נתראה בשמחותB-)<br><span id="names">אלירן ושירן</span></div>
 			</section>
-			<form id="contactUs" action="" method="POST">
-				<label for="userName">שם:</label> <input id="userName"
-					name="userName" type="text" required="required">
-					<div data-role="popup" id="popupNameError" class="error" data-transition="flip">
-					   שם יכול להיות אותיות בעברית או באנגלית
-					</div>
-					 <label for="phone">טלפון:</label> <input
-					type="tel" name="phone" id="phone" required="required">
-					<div data-role="popup" id="popupPhoneError" class="error" data-transition="flip">
-					   מספר טלפון יכול להכיל רק ספרות, - או +
-					</div>
-				<button type="submit" id="send">שלח</button>
-			</form>
+			<section id="left" class="ui-block-b">
+				<iframe src="//www.youtube.com/embed/TgtX9gUzc90?rel=0"></iframe>
+				<section id="formTitle">
+					<br> מלא את הפרטים לקבלת ההטבה:
+				</section>
+				<form id="contactUs" action="" method="POST">
+					<input id="userName" name="userName" type="text"
+						required="required" placeholder="שם:">
+					<div data-role="popup" id="popupNameError" class="error"
+						data-transition="flip">שם יכול להיות אותיות בעברית או באנגלית</div>
+					<input type="tel" name="phone" id="phone" required="required"
+						placeholder="טלפון:">
+					<div data-role="popup" id="popupPhoneError" class="error"
+						data-transition="flip">מספר טלפון יכול להכיל רק ספרות, - או +</div>
+					<button type="submit" id="send">שלח</button>
+				</form>
+			</section>
 		</section>
 		<div class="blueLine"></div>
 		<section id="footer">
-		    <button id="callUs" class="ui-btn ui-corner-all ui-shadow ui-btn-icon-left ui-icon-phone"> צלצלו עכשיו </button>
-		    <span id="details">רונן <a href="tel:050-675-6429">050-675-6429</a></span>
+			<button id="callUs"
+				class="ui-btn ui-corner-all ui-shadow ui-btn-icon-left ui-icon-phone">
+				צלצלו עכשיו</button>
+			<span id="details">רונן <a href="tel:050-675-6429">050-675-6429</a></span>
 		</section>
 		<h2 id="footerSlogen">חוויה של קצב ונשמה</h2>
 	</div>
-	<div data-role="page" data-control-title="thankYou" id="thankyou">תודה על פנייתך אנו ניצור עמך קשר בהקדם</div>
+	<div data-role="page" data-control-title="thankYou" id="thankyou">תודה
+		על פנייתך אנו ניצור עמך קשר בהקדם</div>
 </body>
 </html>
